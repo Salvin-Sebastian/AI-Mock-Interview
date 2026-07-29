30d74aa86ee1552169b6ce65dd2af8fb1343f699
Salvin-Sebastian <salvinsebastian308@gmail.com>
2026-07-29 12:17:33 +0530

    Chnages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.56.1'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups'
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none'
+          }
+        ]
+      }
+    ];
+  },
 };
 
 export default nextConfig;
